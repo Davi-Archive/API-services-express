de7f11250ebd2340621af18f2171aee2664aa989
davi38 <davi4alves@gmail.com>
2022-08-11 02:11:26 -0300

    run dev nodemon

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running in http://localhost:${PORT}`);
+})
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,7 +24,7 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
