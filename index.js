d55aaa4be436159323702b3b7192deb355b390b9
davi38 <davi4alves@gmail.com>
2022-08-11 04:38:41 -0300

    revert

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//HTML
-app.get("/data-hora", function (req, res) {
-  res.sendFile(__dirname + '/views/data-hora.html');
-})
-
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: 'hello APIi' });
