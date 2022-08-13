9f1635ddd00d2b529383d10461c3d68d1b14e8a1
davi38 <davi4alves@gmail.com>
2022-08-13 00:40:31 -0300

    url shortener close to done and mongo db connect.

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+require('dotenv').config()
+
+
+mongoose.connect(process.env.DB_URI);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -13,6 +21,7 @@ app.listen(PORT, () => {
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const shortid = require('shortid');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -65,11 +74,11 @@ app.get("/time/api/:date_string", function (req, res) {
 
 //dados do usuário - /dados/api/whoami
 
-app.get("/dados/api/whoami", function(req,res) {
+app.get("/dados/api/whoami", function (req, res) {
   res.json({
-    "ipaddress" : req.ip || 'not found',
-    "language" : req.headers["accept-language"] || 'not found',
-    "software" : req.headers['user-agent'] || 'not found',
+    "ipaddress": req.ip || 'not found',
+    "language": req.headers["accept-language"] || 'not found',
+    "software": req.headers['user-agent'] || 'not found',
   })
 })
 
@@ -77,12 +86,49 @@ app.get("/dados/api/whoami", function(req,res) {
 //------------------------------------------------
 
 //URL encurta - /url/api/shorturl
+//mongoose connect + SCHEMA
+const ShortURL = mongoose.model('ShortURL', new mongoose.Schema({
+  short_url: String,
+  original_url: String,
+  suffix: String
+}));
 
-app.post("/url/api/shorturl", function(req,res) {
-  res.json({
-    "app" : "url reducer"
+// parse application/json
+var jsonParser = bodyParser.json()
+app.use(bodyParser.json()); //link em json
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/url/api/shorturl', jsonParser, function (req, res) {
+  let client_requested_url = req.body.url
+  let suffix = shortid.generate()
+  let newShortURL = suffix
+
+  let newURL = new ShortURL({
+    short_url: __dirname + "/url/api/shorturl" + suffix,
+    original_url: client_requested_url,
+    suffix: suffix
+  })
+  newURL.save(function (err, result) {
+    if (err) throw err;
+    if (result) {
+      res.json({
+        "short_url": newURL.short_url,
+        "original_url": newURL.original_url,
+        "suffix": newURL.suffix
+      })
+    }
   })
 })
+
+app.get('/url/api/shorturl/:suffix', function(req, res){
+  console.log()
+  res.json({
+    "suffix": req.body.suffix
+  })
+})
+
+
 
 //END URL encurta - /url/api/shorturl
 //------------------------------------------------
@@ -90,20 +136,20 @@ app.post("/url/api/shorturl", function(req,res) {
 
 //Rastreador de Exercícios - /tracker/api/users
 
-app.post("/tracker/api/users/", function(req,res) {
+app.post("/tracker/api/users/", function (req, res) {
   res.json({
     rastreador: "exer"
   })
 })
 
-app.post("/tracker/api/users/:userId/exercises", function(req,res) {
+app.post("/tracker/api/users/:userId/exercises", function (req, res) {
   let passedValue = req.params
   res.json({
     passedValue
   })
 })
 
-app.post("/tracker/api/users//exercises", function(req,res) {
+app.post("/tracker/api/users//exercises", function (req, res) {
   res.json({
     invalid: "construction"
   })
@@ -114,9 +160,9 @@ app.post("/tracker/api/users//exercises", function(req,res) {
 
 //Upload de arquivo - /file/api/fileanalyse
 
-app.post("/file/api/fileanalyse", function(req,res) {
+app.post("/file/api/fileanalyse", function (req, res) {
   res.json({
-    "upload" : "FILE"
+    "upload": "FILE"
   })
 })
 
