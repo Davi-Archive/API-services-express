8e70647c0f6b8db738ef1a45cc6aef49ee54ed64
davi38 <davi4alves@gmail.com>
2022-08-15 22:05:22 -0300

    new html for every API

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json()); //link em json
 app.use(express.static('public'));
 
 // mongo connect
-mongoose.connect(process.env.DB_URI,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const PORT = process.env.PORT || 3000;
@@ -45,6 +45,10 @@ app.get("/api/hello", function (req, res) {
 });
 
 //Data e Hora - /time/api/:date_string
+app.get("/time", (req, res)=>{
+  res.sendFile(__dirname+"/views/time.html")
+})
+
 
 app.get("/time/api", function (req, res) {
   let now = new Date()
@@ -78,6 +82,9 @@ app.get("/time/api/:date_string", function (req, res) {
 })
 //Data Hora FIM - /time/api/:date_string
 //------------------------------------------------
+app.get("/dados/", (req, res) => {
+  res.sendFile(__dirname + "/views/dados.html")
+})
 
 //dados do usuário - /dados/api/whoami
 
@@ -107,7 +114,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/url/api/shorturl/', jsonParser, function (req, res) {
   let client_requested_url = req.body.url
   dns.lookup(urlparser.parse(client_requested_url).hostname, (error, address) => {
-    if (!address){
+    if (!address) {
       res.json({ error: 'invalid url' })
     } else {
       const url = new ShortURL({ url: client_requested_url })
@@ -121,40 +128,42 @@ app.post('/url/api/shorturl/', jsonParser, function (req, res) {
   })
 })
 
-app.get('/url/api/shorturl/:id', function(req, res){
-    const id = req.params.id;
-    ShortURL.findById(id, (err,data) =>{
-      if(!data){
-        res.json({ error: 'invalid url' })
-      } else {
-        res.redirect(data.url)
-      }
-    })
+app.get('/url/api/shorturl/:id', function (req, res) {
+  const id = req.params.id;
+  ShortURL.findById(id, (err, data) => {
+    if (!data) {
+      res.json({ error: 'invalid url' })
+    } else {
+      res.redirect(data.url)
+    }
+  })
 })
 
 
 
 //END URL encurta - /url/api/shorturl
 //------------------------------------------------
-
+app.get("/tracker/", (req, res) => {
+  res.sendFile(__dirname + "/views/tracker.html")
+})
 
 //Rastreador de Exercícios - /tracker/api/users
 const { Schema } = mongoose;
-const userSchema = new Schema ({
+const userSchema = new Schema({
   "username": String,
 })
 
 const exerciseSchema = new Schema({
- "username": String,
- "date": Date,
- "duration": Number,
- "description": String,
+  "username": String,
+  "date": Date,
+  "duration": Number,
+  "description": String,
 })
 
 const logSchema = new Schema({
- "username": String,
- "count": Number,
- "log": Array,
+  "username": String,
+  "count": Number,
+  "log": Array,
 })
 
 // Models
@@ -171,163 +180,163 @@ app.use(express.json())
 app.use(express.static('public'))
 // Api Endpoints
 
- // #1
+// #1
 app.post('/tracker/api/users', (req, res) => {
- UserInfo.find({ "username": req.body.username}, (err, userData) => {
-   if (err) {
-     console.log("Error with server=> ", err)
-   } else {
-     if (userData.length === 0) {
-       const test = new UserInfo({
-         "_id": req.body.id,
-         "username": req.body.username,
-       })
+  UserInfo.find({ "username": req.body.username }, (err, userData) => {
+    if (err) {
+      console.log("Error with server=> ", err)
+    } else {
+      if (userData.length === 0) {
+        const test = new UserInfo({
+          "_id": req.body.id,
+          "username": req.body.username,
+        })
 
-       test.save((err, data) => {
-         if (err) {
-           console.log("Error saving data=> ", err)
-         } else {
-           res.json({
-             "_id": data.id,
-             "username": data.username,
-           })
-         }
-       })
-     } else {
-       res.send("Username already Exists")
-     }
-   }
- })
+        test.save((err, data) => {
+          if (err) {
+            console.log("Error saving data=> ", err)
+          } else {
+            res.json({
+              "_id": data.id,
+              "username": data.username,
+            })
+          }
+        })
+      } else {
+        res.send("Username already Exists")
+      }
+    }
+  })
 })
 
- // #2
+// #2
 app.post('/tracker/api/users/:_id/exercises', (req, res) => {
- let idJson = { "id": req.params._id};
- let checkedDate = new Date(req.body.date);
- let idToCheck = idJson.id;
+  let idJson = { "id": req.params._id };
+  let checkedDate = new Date(req.body.date);
+  let idToCheck = idJson.id;
 
- let noDateHandler = () => {
-   if (checkedDate instanceof Date && !isNaN(checkedDate)) {
-     return checkedDate
-   } else {
-     checkedDate = new Date();
-   }
- }
+  let noDateHandler = () => {
+    if (checkedDate instanceof Date && !isNaN(checkedDate)) {
+      return checkedDate
+    } else {
+      checkedDate = new Date();
+    }
+  }
 
- UserInfo.findById(idToCheck, (err, data) => {
-   noDateHandler(checkedDate);
+  UserInfo.findById(idToCheck, (err, data) => {
+    noDateHandler(checkedDate);
 
-   if (err) {
-     console.log("error with id=> ", err);
-   } else {
-     const test = new ExerciseInfo({
-       "username": data.username,
-       "description": req.body.description,
-       "duration": req.body.duration,
-       "date": checkedDate.toDateString(),
-     })
+    if (err) {
+      console.log("error with id=> ", err);
+    } else {
+      const test = new ExerciseInfo({
+        "username": data.username,
+        "description": req.body.description,
+        "duration": req.body.duration,
+        "date": checkedDate.toDateString(),
+      })
 
-     test.save((err, data) => {
-       if (err) {
-         console.log("error saving=> ", err);
-       } else {
-         console.log("saved exercise successfully");
-         res.json({
-           "_id": idToCheck,
-           "username": data.username,
-           "description": data.description,
-           "duration": data.duration,
-           "date": data.date.toDateString(),
-         })
-       }
-     })
-   }
- })
+      test.save((err, data) => {
+        if (err) {
+          console.log("error saving=> ", err);
+        } else {
+          console.log("saved exercise successfully");
+          res.json({
+            "_id": idToCheck,
+            "username": data.username,
+            "description": data.description,
+            "duration": data.duration,
+            "date": data.date.toDateString(),
+          })
+        }
+      })
+    }
+  })
 })
 
- // change data for test
+// change data for test
 
 app.get('/tracker/api/users/:_id/logs', (req, res) => {
- const { from, to, limit } = req.query;
- let idJson = { "id": req.params._id };
- let idToCheck = idJson.id;
+  const { from, to, limit } = req.query;
+  let idJson = { "id": req.params._id };
+  let idToCheck = idJson.id;
 
- // Check ID
- UserInfo.findById(idToCheck, (err, data) => {
-   var query = {
-     username: data.username
-   }
+  // Check ID
+  UserInfo.findById(idToCheck, (err, data) => {
+    var query = {
+      username: data.username
+    }
 
-   if (from !== undefined && to === undefined) {
-     query.date = { $gte: new Date(from)}
-   } else if (to !== undefined && from === undefined) {
-     query.date = { $lte: new Date(to) }
-   } else if (from !== undefined && to !== undefined) {
-     query.date = { $gte: new Date(from), $lte: new Date(to)}
-   }
+    if (from !== undefined && to === undefined) {
+      query.date = { $gte: new Date(from) }
+    } else if (to !== undefined && from === undefined) {
+      query.date = { $lte: new Date(to) }
+    } else if (from !== undefined && to !== undefined) {
+      query.date = { $gte: new Date(from), $lte: new Date(to) }
+    }
 
-   let limitChecker = (limit) => {
-     let maxLimit = 100;
-     if (limit) {
-       return limit;
-     } else {
-       return maxLimit
-     }
-   }
+    let limitChecker = (limit) => {
+      let maxLimit = 100;
+      if (limit) {
+        return limit;
+      } else {
+        return maxLimit
+      }
+    }
 
-   if (err) {
-     console.log("error with ID=> ", err)
-   } else {
+    if (err) {
+      console.log("error with ID=> ", err)
+    } else {
 
-     ExerciseInfo.find((query), null, {limit: limitChecker(+limit)}, (err, docs) => {
-       let loggedArray = [];
-       if (err) {
-         console.log("error with query=> ", err);
-       } else {
+      ExerciseInfo.find((query), null, { limit: limitChecker(+limit) }, (err, docs) => {
+        let loggedArray = [];
+        if (err) {
+          console.log("error with query=> ", err);
+        } else {
 
-         let documents = docs;
-         let loggedArray = documents.map((item) => {
-           return {
-             "description": item.description,
-             "duration": item.duration,
-             "date": item.date.toDateString()
-           }
-         })
+          let documents = docs;
+          let loggedArray = documents.map((item) => {
+            return {
+              "description": item.description,
+              "duration": item.duration,
+              "date": item.date.toDateString()
+            }
+          })
 
-         const test = new LogInfo({
-           "username": data.username,
-           "count": loggedArray.length,
-           "log": loggedArray,
-         })
+          const test = new LogInfo({
+            "username": data.username,
+            "count": loggedArray.length,
+            "log": loggedArray,
+          })
 
-         test.save((err, data) => {
-           if (err) {
-             console.log("error saving exercise=> ", err)
-           } else {
-             console.log("saved exercise successfully");
-             res.json({
-               "_id": idToCheck,
-               "username": data.username,
-               "count": data.count,
-               "log": loggedArray
-             })
-           }
-         })
-       }
-     })
-   }
- })
+          test.save((err, data) => {
+            if (err) {
+              console.log("error saving exercise=> ", err)
+            } else {
+              console.log("saved exercise successfully");
+              res.json({
+                "_id": idToCheck,
+                "username": data.username,
+                "count": data.count,
+                "log": loggedArray
+              })
+            }
+          })
+        }
+      })
+    }
+  })
 })
 
- // #4
+// #4
 app.get('/tracker/api/users', (req, res) => {
- UserInfo.find({}, (err, data) => {
-   if (err) {
-     res.send("No Users");
-   } else {
-     res.json(data);
-   }
- })
+  UserInfo.find({}, (err, data) => {
+    if (err) {
+      res.send("No Users");
+    } else {
+      res.json(data);
+    }
+  })
 })
 
 //END Rastreador de Exercícios - /tracker/api/users
@@ -336,7 +345,7 @@ app.get('/tracker/api/users', (req, res) => {
 //Upload de arquivo - /file/api/fileanalyse
 //multer({dest: './public/uploads/'}) saves to folder /public/uploads/
 
-app.post("/file/api/fileanalyse", multer().single('upfile'), (req, res)=> {
+app.post("/file/api/fileanalyse", multer().single('upfile'), (req, res) => {
   console.log(req.file)
   res.json({
     name: req.file.originalname,
@@ -345,8 +354,8 @@ app.post("/file/api/fileanalyse", multer().single('upfile'), (req, res)=> {
   })
 })
 
-app.get("/file/", (req, res)=>{
-  res.sendFile(__dirname + '/file/views/index.html');
+app.get("/file/", (req, res) => {
+  res.sendFile(__dirname + '/views/file_upload.html');
 })
 //Upload de arquivo - /file/api/fileanalyse
 //------------------------------------------------
